d066798e709b9deb003f0668f053203c181a9570
Cristian Sarmiento <cccsarmiento@gmail.com>
2015-10-12 15:38:12 -0300

    Draw letters axis

--- js/gp2dfunc.js
+++ js/gp2dfunc.js
@@ -4,7 +4,6 @@ function Gp2dfunc(options) {
     this.Y = options.Y;
     this.ctx = this.canvas.getContext('2d');
     this.ctx.strokeStyle = '#000';
-
     this.tickSize = 5;
     this.range = {
         X: 0,
@@ -14,7 +13,6 @@ function Gp2dfunc(options) {
         X: 0,
         Y: 0
     };
-
     this.range.X = this.X.max - this.X.min;
     this.range.Y = this.Y.max - this.Y.min;
     this.center.Y = Math.abs(this.Y.min / this.range.Y) * this.canvas.height;
@@ -34,11 +32,12 @@ Gp2dfunc.prototype.drawAxis = function() {
     ctx.moveTo(this.center.X, 0);
     ctx.lineTo(this.center.X, this.canvas.height);
     ctx.stroke();
-    //draws ticks in Y axis     
-    ctx.textAlign = 'right';
-    var position = this.canvas.height - this.increment;
+    //draws ticks in Y axis             
+    var position = this.canvas.height - this.increment;    
     var initPos = this.center.X - this.tickSize / 2;
     var unit = -1 * (Math.round((this.canvas.height / this.increment) / 2) - 1);
+    ctx.textAlign = 'right';
+    ctx.fillText('-Y', initPos + this.tickSize +10, (position + this.increment)-5);  
     while (position > 0) {
         ctx.moveTo(initPos, position);
         ctx.lineTo(initPos + this.tickSize, position);
@@ -49,12 +48,16 @@ Gp2dfunc.prototype.drawAxis = function() {
         unit += 1;
         position = Math.round(position - this.increment);
     }
+    ctx.fillText('Y', initPos + this.tickSize + 8, (position + this.increment)-15);      
     //draws ticks in X axis
     initPos = this.center.Y - this.tickSize / 2;
     unit = Math.round((this.canvas.width / this.increment) / 2) - 1;
     ctx.textAlign = 'center';
     ctx.textBaseline = 'top';
+    
     position = this.canvas.width - this.increment;
+  
+    ctx.fillText('X', position + this.increment -10 , initPos -12);
     while (position > 0) {
         ctx.moveTo(position, initPos);
         ctx.lineTo(position, initPos + this.tickSize);
@@ -65,6 +68,7 @@ Gp2dfunc.prototype.drawAxis = function() {
         unit -= 1;
         position = Math.round(position - this.increment);
     }
+    ctx.fillText('-X', position + this.increment -20 , initPos -12);
 }
 
 
