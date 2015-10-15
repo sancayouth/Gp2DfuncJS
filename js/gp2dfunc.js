5928f460401caa945fef02d7e7792783fefe221e
Cristian Sarmiento <cccsarmiento@gmail.com>
2015-10-15 14:10:50 -0300

    Add draw equation

--- js/gp2dfunc.js
+++ js/gp2dfunc.js
@@ -1,91 +1,117 @@
 function Gp2dfunc(options) {
     this.canvas = document.getElementById(options.id);
-    this.X = options.X;
-    this.Y = options.Y;
-    this.ctx = this.canvas.getContext('2d');
-    this.ctx.strokeStyle = '#000';
-    this.tickSize = 5;
-    this.range = {
-        X: 0,
-        Y: 0
-    };
-    this.center = {
-        X: 0,
-        Y: 0
-    };
-    this.range.X = this.X.max - this.X.min;
-    this.range.Y = this.Y.max - this.Y.min;
-    this.center.Y = Math.abs(this.Y.min / this.range.Y) * this.canvas.height;
-    this.center.X = Math.abs(this.X.min / this.range.X) * this.canvas.width;
-    this.increment = this.canvas.width / this.range.X;
-    this.render();
+    if (this.canvas.getContext) {
+        this.X = options.X;
+        this.Y = options.Y;
+        this.ctx = this.canvas.getContext('2d');
+        this.ctx.strokeStyle = '#000';
+        this.tickSize = 5;
+        this.range = {
+            X: 0,
+            Y: 0
+        };
+        this.center = {
+            X: 0,
+            Y: 0
+        };
+        this.range.X = this.X.max - this.X.min;
+        this.range.Y = this.Y.max - this.Y.min;
+        this.center.Y = Math.abs(this.Y.min / this.range.Y) * this.canvas.height;
+        this.center.X = Math.abs(this.X.min / this.range.X) * this.canvas.width;
+        this.increment = this.canvas.width / this.range.X;
+        this.render();
+    } else {
+        throw new Error('Canvas isn\'t supported');
+    }
 }
 
 
 Gp2dfunc.prototype.drawAxis = function() {
-    var ctx = this.ctx;
-    ctx.beginPath();
+    this.ctx.save();
+    this.ctx.beginPath();
     //draws X axis
-    ctx.moveTo(0, this.center.Y);
-    ctx.lineTo(this.canvas.width, this.center.Y);
+    this.ctx.moveTo(0, this.center.Y);
+    this.ctx.lineTo(this.canvas.width, this.center.Y);
     //draws Y axis        
-    ctx.moveTo(this.center.X, 0);
-    ctx.lineTo(this.center.X, this.canvas.height);
-    ctx.stroke();
+    this.ctx.moveTo(this.center.X, 0);
+    this.ctx.lineTo(this.center.X, this.canvas.height);
+    this.ctx.stroke();
     //draws ticks in Y axis             
     var position = this.canvas.height - this.increment;
     var initPos = this.center.X - this.tickSize / 2;
     var unit = -1 * (Math.round((this.canvas.height / this.increment) / 2) - 1);
-    ctx.textAlign = 'right';
-    ctx.fillText('-Y', initPos + this.tickSize + 10, (position + this.increment) - 5);
+    this.ctx.textAlign = 'right';
+    this.ctx.fillText('-Y', initPos + this.tickSize + 10, (position + this.increment) - 5);
     while (position > 0) {
-        ctx.moveTo(initPos, position);
-        ctx.lineTo(initPos + this.tickSize, position);
-        ctx.stroke();
+        this.ctx.moveTo(initPos, position);
+        this.ctx.lineTo(initPos + this.tickSize, position);
+        this.ctx.stroke();
         if (position != this.center.Y) {
-            ctx.fillText(unit, initPos - 2, position);
+            this.ctx.fillText(unit, initPos - 2, position);
         }
         unit += 1;
         position = Math.round(position - this.increment);
     }
-    ctx.fillText('Y', initPos + this.tickSize + 8, (position + this.increment) - 15);
+    this.ctx.fillText('Y', initPos + this.tickSize + 8, (position + this.increment) - 15);
+    this.ctx.restore();
     //draws ticks in X axis
+    this.ctx.save();
     initPos = this.center.Y - this.tickSize / 2;
     unit = Math.round((this.canvas.width / this.increment) / 2) - 1;
-    ctx.textAlign = 'center';
-    ctx.textBaseline = 'top';
+    this.ctx.textAlign = 'center';
+    this.ctx.textBaseline = 'top';
     position = this.canvas.width - this.increment;
-    ctx.fillText('X', position + this.increment - 10, initPos - 12);
+    this.ctx.fillText('X', position + this.increment - 10, initPos - 12);
     while (position > 0) {
-        ctx.moveTo(position, initPos);
-        ctx.lineTo(position, initPos + this.tickSize);
-        ctx.stroke();
+        this.ctx.moveTo(position, initPos);
+        this.ctx.lineTo(position, initPos + this.tickSize);
+        this.ctx.stroke();
         if (position != this.center.X) {
-            ctx.fillText(unit, position, (this.center.Y + this.tickSize / 2) + 2);
+            this.ctx.fillText(unit, position, (this.center.Y + this.tickSize / 2) + 2);
         }
         unit -= 1;
         position = Math.round(position - this.increment);
     }
-    ctx.fillText('-X', position + this.increment - 20, initPos - 12);
+    this.ctx.fillText('-X', position + this.increment - 20, initPos - 12);
+    this.ctx.restore();
     //draw arrows
-    ctx.beginPath();
-    ctx.moveTo(0, this.center.Y);
-    ctx.lineTo(this.tickSize, this.center.Y + this.tickSize);
-    ctx.lineTo(this.tickSize, this.center.Y - this.tickSize);
-    ctx.fill();
-    ctx.moveTo(this.canvas.width, this.center.Y);
-    ctx.lineTo(this.canvas.width - this.tickSize, this.center.Y + this.tickSize);
-    ctx.lineTo(this.canvas.width - this.tickSize, this.center.Y - this.tickSize);
-    ctx.fill();
-    ctx.moveTo(this.center.X, 0);
-    ctx.lineTo(this.center.X + this.tickSize,  this.tickSize);
-    ctx.lineTo(this.center.X - this.tickSize, this.tickSize);
-    ctx.fill();
-    ctx.moveTo(this.center.X, this.canvas.height);
-    ctx.lineTo(this.center.X + this.tickSize,  this.canvas.height-this.tickSize);
-    ctx.lineTo(this.center.X - this.tickSize,  this.canvas.height-this.tickSize);
-    ctx.fill();
-    ctx.save();
+    this.ctx.save();
+    this.ctx.beginPath();
+    this.ctx.moveTo(0, this.center.Y);
+    this.ctx.lineTo(this.tickSize, this.center.Y + this.tickSize);
+    this.ctx.lineTo(this.tickSize, this.center.Y - this.tickSize);
+    this.ctx.fill();
+    this.ctx.moveTo(this.canvas.width, this.center.Y);
+    this.ctx.lineTo(this.canvas.width - this.tickSize, this.center.Y + this.tickSize);
+    this.ctx.lineTo(this.canvas.width - this.tickSize, this.center.Y - this.tickSize);
+    this.ctx.fill();
+    this.ctx.moveTo(this.center.X, 0);
+    this.ctx.lineTo(this.center.X + this.tickSize, this.tickSize);
+    this.ctx.lineTo(this.center.X - this.tickSize, this.tickSize);
+    this.ctx.fill();
+    this.ctx.moveTo(this.center.X, this.canvas.height);
+    this.ctx.lineTo(this.center.X + this.tickSize, this.canvas.height - this.tickSize);
+    this.ctx.lineTo(this.center.X - this.tickSize, this.canvas.height - this.tickSize);
+    this.ctx.fill();
+    this.ctx.restore();
+}
+
+Gp2dfunc.prototype.drawEquation = function(callback) {    
+    this.ctx.save();    
+    this.ctx.translate(this.center.X, this.center.Y);
+    var scale = this.canvas.width / this.range.X;        
+    this.ctx.scale(scale, -scale);
+  
+    this.ctx.beginPath();
+    this.ctx.lineJoin = 'round';    
+    this.ctx.moveTo(this.X.min, callback(this.X.min));    
+    for (var x = this.X.min + 0.1; x <= this.X.max; x += 0.1) {        
+        this.ctx.lineTo(x, callback(x));        
+    }
+    this.ctx.restore();
+    this.ctx.strokeStyle = 'red';         
+    this.ctx.stroke();
+    this.ctx.restore();
 }
 
 Gp2dfunc.prototype.render = function() {
